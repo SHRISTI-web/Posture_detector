b9419c0641c930713fb6710f271687f48dcf9d0e
SHRISTI-web <shristis308@gmail.com>
2023-04-19 02:23:55 +0530

    setting up capture

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+let capture;
+
 function setup(){
     createCanvas(800,500);
+   capture=createCapture(VIDEO);
+   capture.hide();
 }
 
 function draw(){
     background(200);
+
+    image(capture,0,0,800,600);
+    
 }
